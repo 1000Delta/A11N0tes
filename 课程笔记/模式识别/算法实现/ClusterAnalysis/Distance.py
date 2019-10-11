2111cb4bd44e2e6919b4b396a64c72703ede14c4
1000Delta <13975001197@163.com>
2019-10-11 10:07:58 +0800

    2019年 10月 11日 星期五 10:07:58 CST

---
+++ 课程笔记/模式识别/算法实现/ClusterAnalysis/Distance.py
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+import numpy
+
+class Distance:
+
+    def __init__(self, numpy.array x1, numpy.array x2):
+        # 向量维度检查
+        if a.shape != b.shape and len(x1.shape) != 1
+        self.x1 = x1
+        self.x2 = x2
+
+    ''' eculidDistance
+    欧式距离
+    x1 ,x2 为同维度向量
+    '''
+    def eculidDistance(self):
+        # 向量维数判断
+        if len(a.shape) != 1 and a.shape != b.shape:
+            return (0, False)
+        return (1, true)
--- 课程笔记/模式识别/算法实现/ClusterAnalysis/preception.py
+++ 课程笔记/模式识别/算法实现/ClusterAnalysis/preception.py
@@ -6,3 +6,5 @@ preception.py
 
 '''
 
+def perception():
+    
